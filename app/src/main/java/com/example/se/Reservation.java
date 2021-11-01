37b8f01b90441a2295de3f86d569abc0318768f8
Sarah <ysarah0503@gmail.com>
2021-11-01 10:55:12 +0900

    Update Reservation
    Reservation class 변수 및 함수 추가

--- app/src/main/java/com/example/se/Reservation.java
+++ app/src/main/java/com/example/se/Reservation.java
@@ -1,4 +1,33 @@
 package com.example.se;
 
 public class Reservation {
+    private int busId;
+    private int userId;
+    private int reservationId;
+    private static int count = 0;
+
+    Reservation(){
+        reservationId = count;
+        count ++;
+    }
+
+    public void setBusId(int busId) {
+        this.busId = busId;
+    }
+
+    public int getBusId() {
+        return busId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getReservationId() {
+        return reservationId;
+    }
 }
