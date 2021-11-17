487b6243ab9059a3c0b72458257a6de0e393dc43
Sarah <ysarah0503@gmail.com>
2021-11-17 16:26:18 +0900

    Update Reservation_screen
    버스 인원 초과하면 대기 가능

--- app/src/main/java/com/example/se/MainActivity.java
+++ app/src/main/java/com/example/se/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     public static Reservation reservations[] = new Reservation[100];
+    public static User users[] = new User[100];
+    public static Bus buses[] = new Bus[100];
+    public static Waiting waitings[] = new Waiting[100];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
