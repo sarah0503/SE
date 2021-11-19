997fa231ba02b691f0fa5aff8cc6f30d619fb837
Sarah <ysarah0503@gmail.com>
2021-11-20 01:59:13 +0900

    Update infromation_screen
    내정보 확인 창 생성

---
+++ app/src/main/java/com/example/se/Information_screen.java
@@ -0,0 +1,30 @@
+package com.example.se;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.*;
+
+public class Information_screen extends AppCompatActivity {
+    Button modifyButton, withdrawButton, reservationButton, waitingButton, bookmarkButton;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.information_screen);
+        modifyButton = (Button) findViewById(R.id.modifyButton);
+        withdrawButton = (Button) findViewById(R.id.withdrawButton);
+        reservationButton = (Button) findViewById(R.id.reservationButton);
+        waitingButton = (Button) findViewById(R.id.waitingButton);
+        bookmarkButton = (Button) findViewById(R.id.bookmarkButton);
+
+        reservationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
