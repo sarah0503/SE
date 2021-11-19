4d953df3953108138be3b9ec2a21320b3e6892db
Sarah <ysarah0503@gmail.com>
2021-11-19 21:47:51 +0900

    Update ManagerHome
    노선 관리 수정

--- app/src/main/java/com/example/se/Manager_home_screen.java
+++ app/src/main/java/com/example/se/Manager_home_screen.java
@@ -1,17 +1,19 @@
 package com.example.se;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.widget.*;
 
 public class Manager_home_screen extends AppCompatActivity {
-    Button lineButton, noticeButton, LNFButton;
-
+    private Button lineButton, noticeButton, LNFButton;
+    private  String[] selects = {"노선 추가", "노선 변경", "노선 조"};
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +25,30 @@ public class Manager_home_screen extends AppCompatActivity {
         lineButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), AddLine.class);
-                startActivity(intent);
+                final int[] selected = new int[1];
+//                Intent intent = new Intent(getApplicationContext(), AddLine.class);
+//                startActivity(intent);
+                AlertDialog.Builder dialog = new AlertDialog.Builder(Manager_home_screen.this);
+                dialog.setSingleChoiceItems(selects, -1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        selected[0] = which;
+                    }
+                }).setNeutralButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(selected[0] == 0){
+                            Intent intent = new Intent(getApplicationContext(), AddLine.class);
+                            startActivity(intent);
+                        }
+                        else if(selected[0] ==1){
+
+                        }
+                        else if(selected[0] ==2){
+
+                        }
+                    }
+                }).show();
             }
         });
     }
