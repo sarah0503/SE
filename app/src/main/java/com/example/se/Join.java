ae3e6787437bf15f2a7a8841b161743208487def
Sarah <ysarah0503@gmail.com>
2021-11-21 21:48:25 +0900

    Update join
    조인 자바 수정

--- app/src/main/java/com/example/se/Join.java
+++ app/src/main/java/com/example/se/Join.java
@@ -1,10 +1,22 @@
 package com.example.se;
 
-public class Join {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Join extends AppCompatActivity {
     String name;
     int number;
     String password;
     String email;
 
     void error_message(){System.out.print("형식에 맞지 않는 입력입니다.");}
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.join_screen);
+
+    }
 }
