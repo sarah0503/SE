99379ed006a1b562283947735f74a96aade97da7
NohJiin <93262813+NohJiin@users.noreply.github.com>
2021-11-22 01:52:26 +0900

    수정
    쉊ㅇ

---
+++ app/src/main/java/com/example/se/Bookmark.java
@@ -0,0 +1,8 @@
+package com.example.se;
+
+public class Bookmark {
+    Boolean bookmark_on;
+
+    void setBookmark(boolean bookmark_on){this.bookmark_on = bookmark_on;}
+    Boolean getBookmark(){return this.bookmark_on;}
+}
