fd2684f26a20da330215b7ab94eadd5c40ff2bd8
NohJiin <93262813+NohJiin@users.noreply.github.com>
2021-11-24 11:50:28 +0900

    login, join, findpassword
    commit

--- app/src/main/java/com/example/se/Bookmark.java
+++ app/src/main/java/com/example/se/Bookmark.java
@@ -1,6 +1,6 @@
 package com.example.se;
 
-public class Bookmark {
+public class Bookmark {     //DB에서 북마크 정보 가져와야함
     Boolean bookmark_on;
 
     void setBookmark(boolean bookmark_on){this.bookmark_on = bookmark_on;}
