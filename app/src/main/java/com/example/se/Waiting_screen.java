80d90004d2a93cba76f670678c35f6764eb8be8c
NohJiin <93262813+NohJiin@users.noreply.github.com>
2021-11-25 01:53:57 +0900

    modify 수정
    java, xml

--- app/src/main/java/com/example/se/Waiting_screen.java
+++ app/src/main/java/com/example/se/Waiting_screen.java
@@ -10,6 +10,5 @@ public class Waiting_screen extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.waiting_screen);
     }
 }
