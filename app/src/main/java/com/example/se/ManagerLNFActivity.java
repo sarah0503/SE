c033a9d80d48a820e83ff29e42581ce998f50422
dkssud33 <71263290+dkssud33@users.noreply.github.com>
2021-11-28 02:53:07 +0900

    AddLNF -> delete

--- app/src/main/java/com/example/se/ManagerLNFActivity.java
+++ app/src/main/java/com/example/se/ManagerLNFActivity.java
@@ -1,14 +1,14 @@
 package com.example.se;
 
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -21,6 +21,10 @@ public class ManagerLNFActivity extends AppCompatActivity {
 
     private ManagerLNFListAdapter adapter;
     private Button lnfAddButton;
+
+    private Dialog addDialog;
+    private EditText lnfTitle, lnfExplain;
+    private Button lnfSave;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,8 +39,33 @@ public class ManagerLNFActivity extends AppCompatActivity {
         lnfAddButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), AddLNF.class);
-                startActivity(intent);
+                addDialog = new Dialog(view.getContext());
+                addDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                addDialog.setContentView(R.layout.add_lnf_screen);
+                WindowManager.LayoutParams params = addDialog.getWindow().getAttributes();
+                params.width = WindowManager.LayoutParams.MATCH_PARENT;
+                params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+                addDialog.getWindow().setAttributes((WindowManager.LayoutParams) params);
+
+                addDialog.show();
+
+                lnfTitle = addDialog.findViewById(R.id.lnfTitleEditText);
+                lnfExplain = addDialog.findViewById(R.id.lnfExplainEditText);
+                lnfSave = addDialog.findViewById(R.id.lnfSaveButton);
+
+                lnfSave.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        LNF lnf = new LNF();
+                        lnf.setTitle(lnfTitle.getText().toString());
+                        lnf.setExplain(lnfExplain.getText().toString());
+
+                        adapter.addItem(lnf);
+                        adapter.notifyDataSetChanged();
+
+                        addDialog.dismiss();
+                    }
+                });
             }
         });
     }
@@ -59,8 +88,6 @@ public class ManagerLNFActivity extends AppCompatActivity {
         List<String> titleList = Arrays.asList("제목1", "제목2", "제목3", "제목4", "제목5", "제목6", "제목7",
                 "제목8", "제목9", "제목10", "제목11", "제목12");
 
-        //image
-
         List<String> explainList = Arrays.asList(
                 "천마아트센터 스타벅스 \n2021.08.16 \n 스페이스그레이, 핑크색 케이스",
                 "djkfjoiwejklajojf \nkjfdsaeojfo \n kjdfpajkwjf \nskjaiofjkejfio",
@@ -82,7 +109,6 @@ public class ManagerLNFActivity extends AppCompatActivity {
         for(int i = 0; i < titleList.size(); i++) {
             LNF lnf = new LNF();
             lnf.setTitle(titleList.get(i));
-            //lnf.setImage(imageList.get(i));
             lnf.setExplain(explainList.get(i));
 
             adapter.addItem(lnf);
