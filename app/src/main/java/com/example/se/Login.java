96f9cb480de4cbabf6bb61a47b524e045c52684e
NohJiin <93262813+NohJiin@users.noreply.github.com>
2021-11-28 01:13:07 +0900

    Login 수정
    수정

--- app/src/main/java/com/example/se/Login.java
+++ app/src/main/java/com/example/se/Login.java
@@ -56,13 +56,10 @@ public class Login extends AppCompatActivity {
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//              int check = 0;                                      // 없어도 될것 같기두..) DB에서 동일한 아이디를 찾으면 1, 아니면 0
                 String id;                                          // DB에서 가져오는 ID이면 이게 int로 둬야하는지 String으로 둬야하는지..?
                 String pass;
                 id = editTextID.getText().toString();
                 pass = editTextPassword.getText().toString();
-                String loadId ;//보류) DB에서 입력받은 ID와 동일한 것이 있으면 Pass 불러와야함
-                String loadPass ;
                 String URL = "http://yubusin.dothome.co.kr/login_chk.php";
 
                 if(queue == null) {
@@ -108,15 +105,12 @@ public class Login extends AppCompatActivity {
                 if (id.equals("")||pass.equals("")){
                     Toast.makeText(getApplicationContext(), "입력된 값이 없습니다.", Toast.LENGTH_SHORT).show();
                 }
-                else if (!loadId.equals("")){        //공백이 아니라면 동일한 것이 있을것이다....~?
-//                  check = 1;
-                    if (pass.equals(loadPass)){
+                else if (result.equals("success")){
                         Toast.makeText(getApplicationContext(), "로그인되었습니다.", Toast.LENGTH_SHORT).show();
                         Intent intent = new Intent(getApplicationContext(), Home_screen.class);
                         startActivity(intent);
                     }
-                }
-                else if (loadId.equals("")){
+                else {
                     Toast.makeText(getApplicationContext(), "로그인 정보가 잘못되었습니다.", Toast.LENGTH_SHORT).show();
                 }
             }
