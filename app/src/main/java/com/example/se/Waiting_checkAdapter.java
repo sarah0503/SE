09b92442d65d06b57e1d21458886723c3f5e351c
NohJiin <93262813+NohJiin@users.noreply.github.com>
2021-11-28 21:46:32 +0900

    커밋
    커밋

--- app/src/main/java/com/example/se/Waiting_checkAdapter.java
+++ app/src/main/java/com/example/se/Waiting_checkAdapter.java
@@ -46,7 +46,7 @@ public class Waiting_checkAdapter extends BaseAdapter {
         TextView time = (TextView)view.findViewById(R.id.time);
         TextView count = (TextView)view.findViewById(R.id.count);
 
-        busid.setText(waiting.get(position).getBusId());
+        busid.setText(Integer.toString(waiting.get(position).getBusId()));
         userDeparture.setText(waiting.get(position).getDeparture());
         userArrival.setText(waiting.get(position).getArrival());
         time.setText(waiting.get(position).getTime());
